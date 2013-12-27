686fa93e0ff7c00eca8848e84062a46ccc8e39ae
Pete Bunting <6181851+petebunting@users.noreply.github.com>
2013-12-27 11:55:38 +0000

    Update to landsat sensors to remove python3 syntax error.

--- arcsilib/arcsisensorlandsat8.py
+++ arcsilib/arcsisensorlandsat8.py
@@ -374,7 +374,7 @@ class ARCSILandsat8Sensor (ARCSIAbstractSensor):
 
     def run6SToOptimiseAODValue(self, aotVal, radBlueVal, predBlueVal, aeroProfile, atmosProfile, grdRefl, surfaceAltitude):
         """Used as part of the optimastion for identifying values of AOD"""
-        print "Testing AOD Val: ", aotVal
+        print(("Testing AOD Val: ", aotVal))
         s = Py6S.SixS()
         
         s.atmos_profile = atmosProfile
