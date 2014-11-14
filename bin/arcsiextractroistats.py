e70e7e74c809d3ccc751d7666eb57938bf97e4c3
Pete Bunting <petebunting@mac.com>
2014-11-14 16:29:27 +0000

    Add files to extract data for an ROI and plot it. Update so that AOT files have a 'AOT' as the band name. version number incremented.

---
+++ bin/arcsiextractroistats.py
@@ -0,0 +1,263 @@
+#!/usr/bin/env python
+
+
+############################################################################
+#  arcsiextractroistats.py
+#
+#  Copyright 2014 ARCSI.
+#
+#  ARCSI: 'Atmospheric and Radiometric Correction of Satellite Imagery'
+#
+#  ARCSI is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  ARCSI is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with ARCSI.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Purpose:  A class extracts data for an ROI for a set of ARCSI outputted 
+#           image files given an input shapefile.
+#
+# Author: Pete Bunting
+# Email: pfb@aber.ac.uk
+# Date: 05/11/2014
+# Version: 1.0
+#
+# History:
+# Version 1.0 - Created.
+#
+############################################################################
+
+
+# Import the glob module.
+import glob
+# Import python system library
+import sys
+#Import the python file paths module
+import os.path
+# Import the datetime module
+import datetime
+# Import the python Argument parser
+import argparse
+# Import the ARCSI exception class
+from arcsilib.arcsiexception import ARCSIException
+# Import the arcsi version number
+from arcsilib import ARCSI_VERSION
+# Import the RSGISLib Zonal Stats module
+from rsgislib import zonalstats
+
+
+class ARCSIExtractROIStats (object):
+    """
+    A class extracts data for an ROI for a set of ARCSI outputted image files
+    given an input shapefile.
+    """
+
+    def parseDateFromFileName(self, imageFilePath):
+        try:
+            imageFile = os.path.basename(imageFilePath)
+            tokens = imageFile.split("_")
+            dateStr = tokens[1]
+            dateObj = datetime.datetime.strptime(dateStr, "%Y%m%d")
+            return dateObj
+        except ARCSIException as e:
+            raise e
+        except Exception as e:
+            raise e
+
+    def importZonalStatsData(self, statsFile):
+        try:
+            statsData = []
+            stats = open(statsFile, 'r')
+            row = 0
+            for statsRow in stats:
+                statsRow = statsRow.strip()
+                if row > 0:
+                    data = statsRow.split(',')
+                    statsData.append(data)
+                row+=1
+            stats.close()
+            return statsData
+        except ARCSIException as e:
+            raise e
+        except Exception as e:
+            raise e
+    
+    def importZonalStatsFields(self, statsFile):
+        try:
+            statsData = []
+            stats = open(statsFile, 'r')
+            row = 0
+            for statsRow in stats:
+                statsRow = statsRow.strip()
+                if row == 0:
+                    statsData = statsRow.split(',')
+                    break
+            stats.close()
+            return statsData
+        except ARCSIException as e:
+            raise e
+        except Exception as e:
+            raise e
+    
+    def exportStats2TextFile(self, outputFilePath, headerFields, statsData):
+        try:
+            outputFile = open(outputFilePath, 'w')
+            rowStr = ''
+            first = True
+            for field in headerFields:
+                if first:
+                    rowStr = str(field)
+                    first = False
+                else:
+                    rowStr = rowStr + ", " + str(field)
+            rowStr = rowStr + "\n"
+            outputFile.write(rowStr)
+            
+            for data in statsData:
+                first = True
+                for field in data:
+                    if first:
+                        rowStr = field.strftime("%Y%m%d")
+                        first = False
+                    else:
+                        rowStr = rowStr + ", " + str(field)
+                rowStr = rowStr + "\n"
+                outputFile.write(rowStr)
+            
+        except ARCSIException as e:
+            raise e
+        except Exception as e:
+            raise e
+    
+    def extractSingleFileStats(self, tmpOutFile, roiFile, imageFile, pixelIntersectMethod):
+        try:
+            zonalattributes = zonalstats.ZonalAttributes(minThreshold=0, maxThreshold=10000, calcCount=False, calcMin=True, calcMax=True, calcMean=True, calcStdDev=True, calcMode=False, calcSum=False)
+            zonalstats.pixelStats2TXT(imageFile, roiFile, tmpOutFile, zonalattributes, True, True, zonalstats.METHOD_POLYCONTAINSPIXELCENTER, False)
+        except ARCSIException as e:
+            raise e
+        except Exception as e:
+            raise e
+    
+    def sortByDate(self, dateVals, statsData):
+        try:
+            dateVals.sort()
+            statsDataSorted = []
+            for dateVal in dateVals:
+                for statVals in statsData:
+                    if statVals[0].strftime("%Y%m%d") == dateVal.strftime("%Y%m%d"):
+                        statsDataSorted.append(statVals)
+        
+            return statsDataSorted
+        except ARCSIException as e:
+            raise e
+        except Exception as e:
+            raise e
+    
+    def extractImageFileStats(self, inputImagesLoc, outputFile, roiFile):
+        try:
+            inputImages = glob.glob(inputImagesLoc)
+            if len(inputImages) == 0:
+                raise ARCSIException("No input images were found.")
+            statsData2Export = []
+            statsData2ExportFields = []
+            dateVals = []
+            numFields = 0
+            first = True
+            for imageFile in inputImages:
+                print("Processing " + imageFile)
+                if not os.path.exists(imageFile):
+                    raise ARCSIException("Image file does not exist.")
+                self.extractSingleFileStats(outputFile, roiFile, imageFile, zonalstats.METHOD_POLYCONTAINSPIXELCENTER)
+                imageDate = self.parseDateFromFileName(imageFile)
+                dateVals.append(imageDate)
+                if first:
+                    statsData2ExportFields = self.importZonalStatsFields(outputFile)
+                    numFields = len(statsData2ExportFields)
+                    statsData2ExportFields.insert(0, "Date")
+                    first = False
+                statsData = self.importZonalStatsData(outputFile)
+                for stats in statsData:
+                    if len(stats) != numFields:
+                        raise ARCSIException("The number of fields is incorrect.")
+                    stats.insert(0, imageDate)
+                    statsData2Export.append(stats)
+                print("Completed image " + imageDate.strftime("%Y%m%d"))
+            print("Sorting data into date order")
+            statsData2Export = self.sortByDate(dateVals, statsData2Export)
+            print("Exporting Data")
+            self.exportStats2TextFile(outputFile, statsData2ExportFields, statsData2Export)
+            print("Completed Processing.")
+        except ARCSIException as e:
+            raise e
+        except Exception as e:
+            raise e
+    
+    
+if __name__ == '__main__':
+    """
+    The command line user interface to ARCSI Extract ROI Stats tool.
+    """
+    parser = argparse.ArgumentParser(prog='arcsiextractroistats.py',
+                                    description='''ARCSI command for extracting 
+                                                   image pixels stats.''',
+                                    epilog='''A tool to extracts data for an ROI for 
+                                              a set of ARCSI outputted image files
+                                              given an input shapefile.''')
+    # Request the version number.
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s version ' + ARCSI_VERSION)
+    
+    parser.add_argument("-i", "--input", type=str, 
+                        help='''Specify the directory containing the input image files 
+                                and files selected from the input directory, glob 
+                                supported wild characters .''')
+
+    parser.add_argument("-r", "--roi", type=str, 
+                        help='''String to specify the region of interest (ROI). Must be
+                        a shapefile of the image projection as the input images.''')
+                        
+    parser.add_argument("-o", "--output", type=str, 
+                        help='''An output text file with the zonal stats results.''')
+
+    
+    
+
+    # Call the parser to parse the arguments.
+    args = parser.parse_args()
+    
+    if args.input == None:
+        print("Input file information was not specified.")
+        parser.print_help()
+        sys.exit()
+    
+    if args.output == None:
+        print("An output file was not specified.")
+        parser.print_help()
+        sys.exit()
+        
+    if args.roi == None:
+        print("An ROI shapefile was not specified.")
+        parser.print_help()
+        sys.exit()
+    
+    arcsiObj = ARCSIExtractROIStats()
+    arcsiObj.extractImageFileStats(args.input, args.output, args.roi)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
---
+++ bin/arcsiplotextractedstats.py
@@ -0,0 +1,275 @@
+#!/usr/bin/env python
+
+
+############################################################################
+#  arcsiplotextractedstats.py
+#
+#  Copyright 2014 ARCSI.
+#
+#  ARCSI: 'Atmospheric and Radiometric Correction of Satellite Imagery'
+#
+#  ARCSI is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  ARCSI is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with ARCSI.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Purpose:  A class to plot the statistics extracted using the 
+#           arcsiextractroistats.py command.
+#
+# Author: Pete Bunting
+# Email: pfb@aber.ac.uk
+# Date: 05/11/2014
+# Version: 1.0
+#
+# History:
+# Version 1.0 - Created.
+#
+############################################################################
+
+# Import python system library
+import sys
+#Import the python file paths module
+import os.path
+# Import the datetime module
+import datetime
+# Import the python Argument parser
+import argparse
+# Import the ARCSI exception class
+from arcsilib.arcsiexception import ARCSIException
+# Import the arcsi version number
+from arcsilib import ARCSI_VERSION
+# Import matplotlib module
+import matplotlib.pyplot as plt
+import matplotlib.dates as matdate
+
+class ARCSIPlotExtractedStats (object):
+    """
+    A class to plot the statistics extracted using the
+    arcsiextractroistats.py command.
+    """
+    
+    def parseInputFile(self, inputStatsFile):
+        try:
+            statsData = []
+            fieldNames = []
+            inStats = open(inputStatsFile, 'r')
+            row = 0
+            for statsRow in inStats:
+                statsRow = statsRow.strip()
+                if row == 0:
+                    fieldNamesTmp = statsRow.split(',')
+                    for field in fieldNamesTmp:
+                        fieldNames.append(field.strip())
+                else:
+                    rowStrData = statsRow.split(',')
+                    fieldIdx = 0
+                    rowData = []
+                    for data in rowStrData:
+                        data = data.strip()
+                        if fieldIdx == 0:
+                            rowData.append(datetime.datetime.strptime(data, "%Y%m%d"))
+                        elif fieldIdx == 1:
+                            rowData.append(int(data))
+                        elif fieldIdx == (len(rowStrData)-1):
+                            rowData.append(int(data))
+                        else:
+                            rowData.append(float(data))
+                        fieldIdx += 1
+                    statsData.append(rowData)
+                row+=1
+
+            inStats.close()
+            return statsData, fieldNames
+        except ARCSIException as e:
+            raise e
+        except Exception as e:
+            raise e
+    
+    def generatePlot(self, datesVals, minVals, maxVals, avgVals, stdVals, outputPlotFile, fieldBase, plotTitle):
+        try:
+            
+            upperVals = []
+            lowerVals = []
+            
+            for i in range(len(datesVals)):
+                lowVal = avgVals[i] - stdVals[i]
+                if lowVal < minVals[i]:
+                    lowVal = minVals[i]
+                lowerVals.append(lowVal)
+                
+                upVal = avgVals[i] + stdVals[i]
+                if upVal > maxVals[i]:
+                    upVal = maxVals[i]
+                upperVals.append(upVal)
+            
+            fig = plt.figure(figsize=(10, 5), dpi=80)
+            ax1 = fig.add_subplot(111)
+            
+            ax1.plot_date(datesVals, avgVals, 'k-', label='Mean', zorder=10)
+            ax1.fill_between(datesVals, lowerVals, upperVals, alpha=0.2, linewidth=1.0, facecolor='g', edgecolor=[0.70,0.70,0.70], label='Bounds', zorder=-1)
+            
+            ax1Range = ax1.axis('tight')
+            
+            plt.grid(color='k', linestyle='--', linewidth=0.5)
+            plt.title(plotTitle)
+            plt.xlabel("Date")
+            ax1.set_ylabel(fieldBase)
+        
+            plt.savefig(outputPlotFile, format='PDF')
+            
+        except ARCSIException as e:
+            raise e
+        except Exception as e:
+            raise e
+    
+    
+    def processAndPlotData(self, inputStatsFile, outputPlotFile, fieldBase, plotTitle):
+        try:
+            print("Processing Plot \'" + plotTitle + "\'")
+            fieldBase = fieldBase.strip()
+            statsData, fieldNames = self.parseInputFile(inputStatsFile)
+            #print(statsData)
+                        
+            minField = fieldBase + "Min"
+            maxField = fieldBase + "Max"
+            avgField = fieldBase + "Avg"
+            stdField = fieldBase + "Std"
+            dateField = "Date"
+            
+            minFieldIdx = -1
+            maxFieldIdx = -1
+            avgFieldIdx = -1
+            stdFieldIdx = -1
+            dateFieldIdx = -1
+            
+            idx = 0
+            for field in fieldNames:
+                if field == minField:
+                    minFieldIdx = idx
+                elif field  == maxField:
+                    maxFieldIdx = idx
+                elif field == avgField:
+                    avgFieldIdx = idx
+                elif field == stdField:
+                    stdFieldIdx = idx
+                elif field == dateField:
+                    dateFieldIdx = idx
+                idx += 1
+            
+            #print("Date Idx = ", dateFieldIdx)
+            #print("Min Idx = ", minFieldIdx)
+            #print("Max Idx = ", maxFieldIdx)
+            #print("Avg Idx = ", avgFieldIdx)
+            #print("Std Idx = ", stdFieldIdx)
+            
+            if dateFieldIdx == -1:
+                raise ARCSIException("Date Field was not found.")
+            if minFieldIdx == -1:
+                raise ARCSIException("Min Field was not found.")
+            if maxFieldIdx == -1:
+                raise ARCSIException("Max Field was not found.")
+            if avgFieldIdx == -1:
+                raise ARCSIException("Avg Field was not found.")
+            if stdFieldIdx == -1:
+                raise ARCSIException("Std Field was not found.")
+                
+            datesVals = []
+            minVals = []
+            maxVals = []
+            avgVals = []
+            stdVals = []
+            
+            for dataRow in statsData:
+                datesVals.append(dataRow[dateFieldIdx])
+                minVals.append(dataRow[minFieldIdx])
+                maxVals.append(dataRow[maxFieldIdx])
+                avgVals.append(dataRow[avgFieldIdx])
+                stdVals.append(dataRow[stdFieldIdx])
+
+            #print(datesVals)
+            #print(minVals)
+            #print(maxVals)
+            #print(avgVals)
+            #print(stdVals)
+            
+            self.generatePlot(datesVals, minVals, maxVals, avgVals, stdVals, outputPlotFile, fieldBase, plotTitle)
+            print("Completed")
+        except ARCSIException as e:
+            raise e
+        except Exception as e:
+            raise e
+    
+    
+if __name__ == '__main__':
+    """
+    The command line user interface to ARCSI Plot Extracted Data.
+    """
+    parser = argparse.ArgumentParser(prog='arcsiplotextractedstats.py',
+                                    description='''ARCSI command to plot 
+                                                   data.''',
+                                    epilog='''A tool to plot data extracted
+                                              using arcsi.''')
+    # Request the version number.
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s version ' + ARCSI_VERSION)
+    
+    parser.add_argument("-i", "--input", type=str, 
+                        help='''Specify the input statistics file.''')
+
+    parser.add_argument("-f", "--field", type=str, 
+                        help='''String to specify the field to be plotted.
+                                Note, this should be the base name, i.e., 
+                                Red not RedAvg or RedMax''')
+                                
+    parser.add_argument("-t", "--title", type=str, 
+                        help='''String for the title to be added to the plot.''')
+                        
+    parser.add_argument("-o", "--output", type=str, 
+                        help='''Output PDF file for the plot.''')
+
+    
+    # Call the parser to parse the arguments.
+    args = parser.parse_args()
+    
+    if args.input == None:
+        print("Input file information was not specified.")
+        parser.print_help()
+        sys.exit()
+    
+    if args.output == None:
+        print("An output file was not specified.")
+        parser.print_help()
+        sys.exit()
+        
+    if args.field == None:
+        print("An input field needs to be specified.")
+        parser.print_help()
+        sys.exit()
+    
+    if args.title == None:
+        print("A title for the plot is required.")
+        parser.print_help()
+        sys.exit()
+    
+    arcsiObj = ARCSIPlotExtractedStats()
+    arcsiObj.processAndPlotData(args.input, args.output, args.field, args.title)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
